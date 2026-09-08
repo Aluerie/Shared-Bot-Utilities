0770c2b6f1cc963c2dcaecce6d984798d63967e8
Aluerie <aluerie@gmail.com>
2026-09-08 19:18:22 +0300

    fix(lazy): Various fixes & updates

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
+"""
+Shared Bot Utilities.
+
+This package contains a bunch of utilities, helpers, small API wrappers that I use for some of my projects.
+
+License
+-------
+* This Source Code Form is subject to the terms of the [Mozilla Public License v2.0](<http://mozilla.org/MPL/2.0/>).
+* Copyright (C) 2020-present [@Aluerie](<https://github.com/Aluerie>).
+"""
+
 from . import errors as errors, seven_tv as seven_tv
