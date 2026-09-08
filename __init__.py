312cae08fceec5e301398b574e4e27a505438389
Aluerie <aluerie@gmail.com>
2026-09-08 03:45:12 +0300

    Maybe this is better

---
+++ __init__.py
@@ -0,0 +1,33 @@
+from . import (
+    async_config as async_config,
+)
+from . import (
+    cache as cache,
+)
+from . import (
+    checks as checks,
+)
+from . import (
+    converters as converters,
+)
+from . import (
+    db as db,
+)
+from . import (
+    fuzzy as fuzzy,
+)
+from . import (
+    markdown as markdown,
+)
+from . import (
+    nihongo as nihongo,
+)
+from . import (
+    paginator as paginator,
+)
+from . import (
+    time as time,
+)
+from . import (
+    ui as ui,
+)
